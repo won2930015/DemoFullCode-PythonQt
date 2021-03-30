b84a52450d5b4c0369384f4f674cd18b681fc8db
won2930015 <58769598@qq .com>
2021-03-30 11:06:27 +0800

    ...

--- chap07Database/Demo7_1Table/myMainWindow.py
+++ chap07Database/Demo7_1Table/myMainWindow.py
@@ -17,9 +17,9 @@ from myDelegates import QmyComboBoxDelegate
 class QmyMainWindow(QMainWindow): 
 
    def __init__(self, parent=None):
-      super().__init__(parent)   #调用父类构造函数，创建窗体
-      self.ui=Ui_MainWindow()    #创建UI对象
-      self.ui.setupUi(self)      #构造UI界面
+      super().__init__(parent)   # 调用父类构造函数，创建窗体
+      self.ui=Ui_MainWindow()    # 创建UI对象
+      self.ui.setupUi(self)      # 构造UI界面
 
       self.setCentralWidget(self.ui.splitter)
 
@@ -80,7 +80,7 @@ class QmyMainWindow(QMainWindow):
 ##创建界面组件与数据模型的字段之间的数据映射
       self.mapper= QDataWidgetMapper()
       self.mapper.setModel(self.tabModel) #设置数据模型
-      self.mapper.setSubmitPolicy(QDataWidgetMapper.AutoSubmit)  # 1.提交策略，2.自动提交.
+      self.mapper.setSubmitPolicy(QDataWidgetMapper.AutoSubmit)  # 1.提交策略，2.自动提交
     
 ##界面组件与tabModel的具体字段之间的联系
       self.mapper.addMapping(self.ui.dbSpinEmpNo, self.fldNum["empNo"])
